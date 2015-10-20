e564f39da0bd1e9575b1f8287e44e410dfdc2428
jackjayanth <jayanth.rymec@gmail.com>
2015-10-20 21:55:20 +0000

    added a new action to controller and tests and error messages little complicated

--- fakebook/config/routes.rb
+++ fakebook/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   get 'static_pages/about'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
