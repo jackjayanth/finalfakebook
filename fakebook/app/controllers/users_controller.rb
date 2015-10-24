186c4311939552297fa2c7c6a9c172972d7eeff0
jackjayanth <jayanth.rymec@gmail.com>
2015-10-24 09:58:46 +0000

    perfectly working login and signup plus u can update password n email n see all users with all navigations working

--- fakebook/app/controllers/users_controller.rb
+++ fakebook/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :correct_user,  only: [:edit, :update]
+  
+  def index
+    @users= User.all
+    
+  end
   def show
     @user= User.find(params[:id])
   end
@@ -20,10 +27,37 @@ class UsersController < ApplicationController
     end 
   end
   
+  def edit
+    
+    # @user= User.find(params[:id])
+  end
+  
+  def update
+  # @user= User.find(params[:id])
+      if @user.update_attributes(user_params)
+         redirect_to @user
+      else
+        render 'edit'
+      end
+  
+  end
+  
+
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+      redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
   
   
--- fakebook/app/helpers/sessions_helper.rb
+++ fakebook/app/helpers/sessions_helper.rb
@@ -18,4 +18,10 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user=nil
     end
+    
+    def current_user?(user)
+    
+        user=current_user
+        
+    end
 end
