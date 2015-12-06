f4f0d62f2dae31a18462d735d21df3aa250cd939
jackjayanth <jayanth.rymec@gmail.com>
2015-12-06 13:29:58 +0000

    user interface done

--- fakebook/config/routes.rb
+++ fakebook/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get 'logout'  => 'sessions#destroy'
   resources :users 
   resources :microposts,only: [:create, :destroy]
   
